e305c1851be6b9aa0060382910b5b25dad2f6a90
youssab <youssabyasser@gmail.com>
2025-01-26 14:54:04 +0200

    Project Created

---
+++ Sports App/Sports App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sports App
+//
+//  Created by Yasser Yasser on 26/01/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sports App/Sports AppTests/Sports_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sports_AppTests.swift
+//  Sports AppTests
+//
+//  Created by Yasser Yasser on 26/01/2025.
+//
+
+import Testing
+@testable import Sports_App
+
+struct Sports_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
