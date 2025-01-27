a6ca166eb0763dd458be187d2671e22a3f46abce
youssab <youssabyasser@gmail.com>
2025-01-27 16:49:43 +0200

    New root view controller is working correctly

--- Sports App/Sports App/View/FavouriteViewController.swift
+++ Sports App/Sports App/View/FavouriteViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  FavouriteViewController.swift
 //  Sports App
 //
 //  Created by Yasser Yasser on 26/01/2025.
@@ -7,13 +7,15 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FavouriteViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
     
+
     /*
     // MARK: - Navigation
 
