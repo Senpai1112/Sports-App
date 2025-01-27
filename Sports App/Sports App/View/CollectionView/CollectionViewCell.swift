3b513b1068a7d31d9bf2c9b4da27248605e37e84
youssab <youssabyasser@gmail.com>
2025-01-27 22:44:10 +0200

    Tab Bar Controller UI works correctly

---
+++ Sports App/Sports App/View/CollectionView/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  Sports App
+//
+//  Created by Yasser Yasser on 27/01/2025.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var sportName: UILabel!
+    @IBOutlet weak var sportImage: UIImageView!
+}
